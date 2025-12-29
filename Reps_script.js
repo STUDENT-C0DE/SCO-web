85ec27724e94ff3cf2eb3bd3b1a2dd6432b8f68f
STUDENT-C0DE <andre.potski35@gmail.com>
2025-12-29 21:22:37 +0800

    Create Reps_script.js

---
+++ Reps_script.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper('.slider_wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+  }
+});
